8bcd555b22aba9aab6be8dd37fe6632de9f60198
sidharth980 <kidsachu@gmail.com>
2024-01-01 20:11:01 +0530

    OOP

--- src/main/java/org/example/FourBall.java
+++ src/main/java/org/example/FourBall.java
@@ -3,10 +3,26 @@ import processing.core.PApplet;
 
 public class FourBall extends PApplet{
     private final int screenHeight = 500;
-    private int distance = 0;
     private final int screenWidth = 640;
     private final int radi = 5;
     private int balls = 4;
+
+    class Ball{
+        private int distance = 0;
+        private final int speed;
+
+        private final int position;
+
+        Ball(int speed,int position){
+            this.speed = speed;
+            this.position= position;
+        }
+        void drawBall(){
+            ellipse(distance,(screenHeight/5)*position,radi,radi);
+            distance += speed;
+        }
+    }
+    private Ball[] ball = new Ball[balls];
 
     @Override
     public void settings() {
@@ -14,10 +30,16 @@ public class FourBall extends PApplet{
     }
 
     @Override
+    public void setup() {
+        for(int pos = 0;pos<balls;pos++){
+            ball[pos] = new Ball(pos+1,pos+1);
+        }
+    }
+
+    @Override
     public void draw() {
-        for(int pos = 1; pos<=balls;pos++){
-             ellipse(distance*pos,pos*(screenHeight/5),radi,radi);
-             distance+=1;
+        for(int pos = 0;pos<balls;pos++){
+            ball[pos].drawBall();
         }
     }
     public static void main(String[] args) {PApplet.main("org.example.FourBall",args);}
